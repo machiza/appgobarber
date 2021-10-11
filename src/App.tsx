f3dacc0aa68ded298265d57e93f948a2cf182ee0
machiza <machizaarmando@gmail.com>
2021-10-11 09:34:04 +0200

    Autenticação de Usuários

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
+import AppProvider from './hooks';
+
 import Routes from './routes';
 
 const App: React.FC = () => (
   <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-    <GestureHandlerRootView style={{ flex: 1, backgroundColor: '#312e38' }}>
-      <Routes />
-    </GestureHandlerRootView>
+    <AppProvider>
+      <GestureHandlerRootView style={{ flex: 1, backgroundColor: '#312e38' }}>
+        <Routes />
+      </GestureHandlerRootView>
+    </AppProvider>
   </NavigationContainer>
 );
 
